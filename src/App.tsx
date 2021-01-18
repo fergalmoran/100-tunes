82e786d04f9b322cab194d605451bb0dfacc2161
Fergal Moran <fergal.moran@gmail.com>
2021-01-18 12:51:01 +0000

    Nicer layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,68 @@
-import { Typography } from "@material-ui/core";
+import {
+    AppBar,
+    Container,
+    CssBaseline,
+    makeStyles,
+    Toolbar,
+    Typography,
+} from "@material-ui/core";
 import React from "react";
 import "./App.css";
 import TuneList from "./components/TuneList";
+import AlbumIcon from "@material-ui/icons/Album";
+import { Tune } from "@material-ui/icons";
+
+const useStyles = makeStyles((theme) => ({
+    icon: {
+        marginRight: theme.spacing(2),
+    },
+    heroContent: {
+        backgroundColor: theme.palette.background.paper,
+        padding: theme.spacing(8, 0, 6),
+    },
+    heroButtons: {
+        marginTop: theme.spacing(4),
+    },
+    cardGrid: {
+        paddingTop: theme.spacing(8),
+        paddingBottom: theme.spacing(8),
+    },
+    card: {
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+    },
+    cardMedia: {
+        paddingTop: "56.25%", // 16:9
+    },
+    cardContent: {
+        flexGrow: 1,
+    },
+    footer: {
+        backgroundColor: theme.palette.background.paper,
+        padding: theme.spacing(6),
+    },
+}));
 
 function App() {
+    const classes = useStyles();
     return (
-        <div className="App">
-            <Typography
-                component="h2"
-                variant="h6"
-                color="primary"
-                gutterBottom
-            >
-                100 Tunes
-            </Typography>
-            <TuneList />
-        </div>
+        <React.Fragment>
+            <CssBaseline />
+            <AppBar position="relative">
+                <Toolbar>
+                    <AlbumIcon className={classes.icon} />
+                    <Typography variant="h6" color="inherit" noWrap>
+                        100 Tunes!!!
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <main>
+                <Container maxWidth="xl">
+                    <TuneList />
+                </Container>
+            </main>
+        </React.Fragment>
     );
 }
 
--- src/components/TuneList.tsx
+++ src/components/TuneList.tsx
@@ -1,5 +1,6 @@
 import {
     Avatar,
+    Link,
     Table,
     TableBody,
     TableCell,
@@ -15,7 +16,7 @@ const MixList = () => {
     useEffect(() => {
         const getData = async () => {
             try {
-                const mixes: Tune[] = await(
+                const mixes: Tune[] = await (
                     await fetch(`${process.env.PUBLIC_URL}/data/tunes.json`)
                 ).json();
                 setTunes(mixes);
@@ -31,20 +32,28 @@ const MixList = () => {
             <Table size="small">
                 <TableHead>
                     <TableRow>
-                        <TableCell>Link</TableCell>
+                        <TableCell align="right"></TableCell>
                         <TableCell>Title</TableCell>
-                        <TableCell align="right">Preview</TableCell>
+                        <TableCell>Link</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
                     {tunes.map((row) => (
                         <TableRow key={row.id}>
-                            <TableCell>{row.url}</TableCell>
+                            <TableCell align="center">
+                                <Avatar alt={row.title} src={row.image} />
+                            </TableCell>
                             <TableCell>
                                 <TuneTitle url={row.title} />
                             </TableCell>
-                            <TableCell align="right">
-                                <Avatar alt={row.title} src={row.image} />
+                            <TableCell>
+                                <Link
+                                    href={row.url}
+                                    color="inherit"
+                                    variant="body2"
+                                >
+                                    Open
+                                </Link>
                             </TableCell>
                         </TableRow>
                     ))}
